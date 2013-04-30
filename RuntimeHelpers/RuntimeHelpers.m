5fe8aa9fb52f11bf92a0b320226a102fe7e0d5d2
Fernando Mazzon <Fernando.Mazzon@rga.com>
2013-04-30 14:14:53 -0300

    Initial Commit

---
+++ RuntimeHelpers/RuntimeHelpers.m
@@ -0,0 +1,13 @@
+//
+//  RuntimeHelpers.m
+//  RuntimeHelpers
+//
+//  Created by Fernando Mazzon on 4/30/13.
+//  Copyright (c) 2013 Fernando Mazzon. All rights reserved.
+//
+
+#import "RuntimeHelpers.h"
+
+@implementation RuntimeHelpers
+
+@end
